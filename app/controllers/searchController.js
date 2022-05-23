414ce832dd128a9f82792142d93af3c223525ebd
Charly Morand <charly,morand@oclock.io>
2022-05-23 15:17:16 +0200

    fixed indentation + added comments

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -13,16 +13,21 @@ const searchController = {
    */
    searchCardByElement: async (req, res, next) =>{
     const cardElement = req.query.element;
+    // ici je me suis permet de réajuster l'indentation pour que ça soit un peu plus lisible, rien de méchant :p
     if(cardElement == 'null') {
       try {
+        // penses bien a nommer tes variables au pluriel si tu récuperes plusieurs objets du meme type, ici plusieurs cartes donc cardsWithoutElement aurait été un meilleur choix ;)
         const cardWithoutElement = await dataMapper.getCardWithoutElement()
         res.render('element', {cards : cardWithoutElement})
-      } catch (error) {
+      }
+      catch (error) {
         next()
-      }} else {        
-        const cardWithElement = await dataMapper.getCardByElement(cardElement)
-        res.render('element', {cards : cardWithElement})  
-      }  
+      }
+    } else {
+      // meme remarque ici pour le pluriel au nommage
+      const cardWithElement = await dataMapper.getCardByElement(cardElement)
+      res.render('element', {cards : cardWithElement})
+    }
   }
 };
 
